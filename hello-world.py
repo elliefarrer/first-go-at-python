adfb151b7f90585193ea1f4b996c6c53b01408bb
Ellie Farrer <elliefarrermusic@gmail.com>
2018-10-03 07:48:26 +0100

    Initial commit - had a go at codeacademy tip calculator

---
+++ hello-world.py
@@ -0,0 +1,18 @@
+#First go
+print("Hello, World!")
+
+message = "Hello, World!"
+print(message)
+#-------> print "Hello World!" is Python 2 syntax, use print("Hello World!") for Python 3
+
+#Codeacademy calculator
+meal = 44.5
+print("Initial meal price is", meal)
+tax = 6.75/100
+tip = 15.0/100
+
+meal = meal + meal * tax
+print("Meal price with tax is", meal)
+
+total = meal + (meal * tip)
+print("Total is", total)
